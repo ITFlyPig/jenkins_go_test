66bc4d885d395b4f62b6df32e21ec6ae84c83dfe
a1659509224 <a1659509224@163.com>
2022-06-10 18:05:28 +0800

    添加dockerfile

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module xiaobao
 
 go 1.13
+
+require github.com/labstack/echo/v4 v4.7.2
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/labstack/echo/v4"
 
 func main() {
-	fmt.Println("hello")
+	e := echo.New()
+	// 配置路由
+	e.GET("/hellow", func(c echo.Context) error {
+		//return c.JSON(100, "hello")
+		return c.String(200, "hello")
+	})
+	// 启动Web服务
+	e.Start(":8090")
 }
